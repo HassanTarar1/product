33d54d95ec121a48a40e9dc7e9f525a75e10355a
Hassan ijaz <h.ijaz@it22.nl>
2023-04-09 23:40:27 +0500

    Initial commit

---
+++ src/main/java/com/example/product/ProductService.java
@@ -0,0 +1,42 @@
+package com.example.product;
+
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("resouce not found") );
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product product) {
+        Product existingProduct = getProductById(id);
+        existingProduct.setName(product.getName());
+        existingProduct.setDescription(product.getDescription());
+        existingProduct.setPrice(product.getPrice());
+        return productRepository.save(existingProduct);
+    }
+
+    public void deleteProduct(Long id) {
+        getProductById(id);
+        productRepository.deleteById(id);
+    }
+}
+
